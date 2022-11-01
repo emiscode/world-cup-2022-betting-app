1f42d05f628ce0c68b2840cdda08f39f9f704701
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-11-01 12:08:35 -0300

    add rest client and adjust cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import * as dotenv from "dotenv";
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 
 dotenv.config({ path: __dirname + "/.env" });
 
 const port = Number(process.env.PORT) || 3333;
+const host = String(process.env.HOST) || "0.0.0.0";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -15,13 +17,17 @@ async function bootstrap() {
     logger: true,
   });
 
+  await fastify.register(cors, {
+    origin: true,
+  });
+
   fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count();
 
     return { count };
   });
 
-  await fastify.listen({ port });
+  await fastify.listen({ port, host });
 }
 
 bootstrap();
