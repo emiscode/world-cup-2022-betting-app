0a54bb5045506244bb9c5394e30cef619ad54bf0
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-11-01 11:57:05 -0300

    config prisma and count results

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,24 @@
 import * as dotenv from "dotenv";
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config({ path: __dirname + "/.env" });
 
 const port = Number(process.env.PORT) || 3333;
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get("/bets/count", () => {
-    return { count: 10 };
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
   });
 
   await fastify.listen({ port });
