fceee4a3234550976a7efb89519edd7e8d1b1cde
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-11-03 07:03:12 -0300

    create html structure

--- client-web/src/pages/index.tsx
+++ client-web/src/pages/index.tsx
@@ -1,8 +1,12 @@
 import * as dotenv from "dotenv";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { NextResponse } from "next/server";
-import { useState } from "react";
+
+import Image from "next/image";
+import appPreviewImg from "../assets/app-preview.png";
+import logoImg from "../assets/logo.svg";
+import usersAvatarImg from "../assets/users-avatar.png";
+import iconCheckImg from "../assets/icon-check.svg";
 
 interface Pools {
   count: number;
@@ -13,17 +17,50 @@ interface Pools {
 
 export default function Home(props: Pools) {
   const { t } = useTranslation("common");
-  const [lang, setLang] = useState(props.locale);
   const URL = `http://${props.clientHost}:${props.clientPort}`;
 
   return (
     <div>
-      <a href={`${URL}/pt`}>{t("lang-pt")}</a> |{" "}
-      <a href={`${URL}/en`}>{t("lang-en")}</a>
-      <h1>{t("title")}</h1>
-      <p>
-        {t("pools")}: {props.count}
-      </p>
+      <main>
+        <a href={`${URL}/pt`}>{t("lang-pt")}</a> |{" "}
+        <a href={`${URL}/en`}>{t("lang-en")}</a>
+        <Image src={logoImg} alt="logo bet copa" />
+        <h1>{t("title")}</h1>
+        <div>
+          <Image src={usersAvatarImg} alt="" />
+          <strong>
+            <span>+8.000</span> pessoas já estão jogando
+          </strong>
+        </div>
+        <form>
+          <input type="text" required placeholder="Qual o nome do seu bolão" />
+          <button type="submit">Criar meu bolão</button>
+        </form>
+        <p>
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar outras pessoas
+        </p>
+        <div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.000</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+100.000</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+        <p>
+          {t("pools")}: {props.count}
+        </p>
+      </main>
+      <Image src={appPreviewImg} alt="app preview" quality={100} />
     </div>
   );
 }
