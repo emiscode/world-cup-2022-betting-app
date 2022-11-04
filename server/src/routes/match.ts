3eca818a77486bd5c8509b8e9f39899905844190
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-11-04 18:42:08 -0300

    list match from a pool

--- server/src/routes/match.ts
+++ server/src/routes/match.ts
@@ -1,4 +1,45 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
+import { z } from "zod";
 
-export async function matchRoutes(fastify: FastifyInstance) {}
+import { authenticate } from "../plugins/authenticate";
+
+export async function matchRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    "/pools/:id/match",
+    { onRequest: [authenticate] },
+    async (request) => {
+      const getPoolsParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPoolsParams.parse(request.params);
+
+      const matchList = await prisma.match.findMany({
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          bets: {
+            where: {
+              bettor: {
+                userId: request.user.sub,
+                poolId: id,
+              },
+            },
+          },
+        },
+      });
+
+      return {
+        match: matchList.map((game) => {
+          return {
+            ...game,
+            guess: game.bets.length > 0 ? game.bets[0] : null,
+            guesses: undefined,
+          };
+        }),
+      };
+    }
+  );
+}
