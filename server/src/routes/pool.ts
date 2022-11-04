e9d66ca9b3fc2f624d24a0ce9de39274af3c31a4
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-11-04 16:52:20 -0300

    find pool by id feature

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -142,4 +142,45 @@ export async function poolRoutes(fastify: FastifyInstance) {
 
     return { pools };
   });
+
+  fastify.get("/pools/:id", { onRequest: [authenticate] }, async (request) => {
+    const getPoolsParams = z.object({
+      id: z.string(),
+    });
+
+    const { id } = getPoolsParams.parse(request.params);
+
+    const pool = await prisma.pool.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        _count: {
+          select: {
+            bettors: true,
+          },
+        },
+        bettors: {
+          select: {
+            id: true,
+
+            user: {
+              select: {
+                avatarUrl: true,
+              },
+            },
+          },
+          take: 4,
+        },
+        owner: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    return { pool };
+  });
 }
